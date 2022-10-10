0650a0aec1895ad20b6e7852bfb83535b30fcb4e
inukares <zelowski@gmail.com>
2022-10-10 12:57:40 +0200

    display and test happy path with css grid table

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ const RESULTS_PER_REQUEST = 10;
 function App() {
   const [data, setData] = useState();
   const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     // todo: handle cleanup if http call fails in the meantime of updating component
     const doFetch = async () => {
+      setLoading(true);
       const url = EXCHANGES_URL + `?per_page=${RESULTS_PER_REQUEST}`;
       try {
         const res = await axios.get(url);
         setData(res.data);
+        setLoading(false);
       } catch (error) {
         if (error) console.log(error.toJson());
         setError(error);
@@ -27,7 +30,27 @@ function App() {
 
   return (
     <div className="App">
-      <div>hi</div>
+      {loading ? <span>loading</span> : null}
+      {data && (
+        <div className="container">
+          {data.map((exchange, index) => (
+            <div key={exchange.name ?? index} className="item">
+              <div> {exchange.name}</div>
+              <img
+                data-test-id={exchange.name + '-img'}
+                alt={exchange.name}
+                src={exchange.image}
+              ></img>
+              <div> {exchange.year_established}</div>
+              <div> {exchange.country}</div>
+              <div> {exchange.has_trading_incentive ? 'Yes' : 'No'}</div>
+              <div> {exchange.trust_score}</div>
+              <div> {exchange.trust_score_rank}</div>
+              <div> {exchange.trade_volume_24h_btc_normalized}</div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,9 +1,44 @@
 import { test, expect } from '@playwright/experimental-ct-react';
 import App from './App';
+import { mock, missingData } from './mockResponse';
 
 test.use({ viewport: { width: 500, height: 500 } });
 
-test('should work', async ({ mount }) => {
+// TODO: Test for routing logic
+test('should display perfect path', async ({ mount }) => {
   const component = await mount(<App></App>);
-  await expect(component).toContainText('hi');
+  await expect(component).toContainText('loading');
+  for (let i = 0; i < mock.length; i++) {
+    await expect(component).toContainText(mock[i].name);
+    await expect(component).toContainText(mock[i].country);
+    await expect(component).toContainText(mock[i].description);
+    // await expect(component).toContainText(mock[i].url);
+    await expect(
+      component.locator(`data-test-id=${mock[i].name + '-img'}`)
+    ).toBeVisible();
+    await expect(component).toContainText(mock[i].has_trading_incentive);
+    await expect(component).toContainText(mock[i].trust_score);
+    await expect(component).toContainText(mock[i].trust_score_rank);
+    await expect(component).toContainText(mock[i].trade_volume_24h_btc);
+    await expect(component).toContainText(
+      mock[i].trade_volume_24h_btc_normalized
+    );
+  }
+});
+
+// TODO: add mocking http call and providing default values
+test.skip('should fallback to default values with insufficient data', async ({
+  mount,
+}) => {
+  const component = await mount(<App></App>);
+  await expect(component).toContainText('loading');
+  const mock = missingData[0];
+  await expect(component).toContainText('-');
+  await expect(component).toContainText('Not specified');
+  await expect(component).toContainText('-');
+  await expect(component).toContainText('No');
+  await expect(component).toContainText('-');
+  await expect(component).toContainText('-');
+  await expect(component).toContainText('Unknown');
+  await expect(component).toContainText('Unknown');
 });
