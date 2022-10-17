e452f0b6f11cee4191c0ecf8f51c436de9377b15
inukares <zelowski@gmail.com>
2022-10-17 12:53:25 +0200

    realised I'm dumb and you can fetch exchange by id

--- src/routes/exchange/Exchange.js
+++ src/routes/exchange/Exchange.js
@@ -1,5 +1,5 @@
 import './Exchange.css';
-import { getSanitizedExchange } from './utils';
+import { getExchangeFromURL, getSanitizedExchange } from './utils';
 
 export const formatTradeVolume = (volumeNormalised, volume) => {
   if (volumeNormalised) return volumeNormalised;
@@ -13,48 +13,37 @@ export const formatTradeVolume = (volumeNormalised, volume) => {
 // that way user would've been informed that something went wrong
 
 export const Exchange = () => {
-  const {
-    name,
-    year_established,
-    country,
-    description,
-    has_trading_incentive,
-    image,
-    trade_volume_24h_btc,
-    trade_volume_24h_btc_normalized,
-    trust_score,
-    trust_score_rank,
-    url,
-  } = getSanitizedExchange();
+  const id = getExchangeFromURL();
+  console.log(id);
 
-  const content = (
-    <>
-      <div>Name</div>
-      <div>{name}</div>
-      <div>Year Established</div>
-      <div>{year_established}</div>
-      <div>Country</div>
-      <div>{country}</div>
-      <div>Description</div>
-      <div>{description}</div>
-      <div>Incentivies Trading</div>
-      <div>{has_trading_incentive}</div>
-      <div>Logo</div>
-      {image ? <img src={image} /> : <span>Not available</span>}
-      <div>BTC 24H Volume</div>
-      <div>
-        {formatTradeVolume(
-          trade_volume_24h_btc,
-          trade_volume_24h_btc_normalized
-        )}
-      </div>
-      <div>Trust Score</div>
-      <div>{trust_score}</div>
-      <div>Trust Score Rank</div>
-      <div>{trust_score_rank}</div>
-      <div>Url</div>
-      <div>{url}</div>
-    </>
-  );
-  return <div className="wrapper">{content}</div>;
+  // const content = (
+  // <>
+  //   <div>Name</div>
+  //   <div>{name}</div>
+  //   <div>Year Established</div>
+  //   <div>{year_established}</div>
+  //   <div>Country</div>
+  //   <div>{country}</div>
+  //   <div>Description</div>
+  //   <div>{description}</div>
+  //   <div>Incentivies Trading</div>
+  //   <div>{has_trading_incentive}</div>
+  //   <div>Logo</div>
+  //   {image ? <img src={image} /> : <span>Not available</span>}
+  //   <div>BTC 24H Volume</div>
+  //   <div>
+  //     {formatTradeVolume(
+  //       trade_volume_24h_btc,
+  //       trade_volume_24h_btc_normalized
+  //     )}
+  //   </div>
+  //   <div>Trust Score</div>
+  //   <div>{trust_score}</div>
+  //   <div>Trust Score Rank</div>
+  //   <div>{trust_score_rank}</div>
+  //   <div>Url</div>
+  //   <div>{url}</div>
+  // </>
+  // );
+  return <div className="wrapper">{id}</div>;
 };
--- src/routes/exchange/utils.js
+++ src/routes/exchange/utils.js
@@ -1,51 +1,6 @@
 import { getURLParams } from '../../shared/getURLParams';
-import { sanitizeParams } from '../../shared/sanitizeParams';
-
-import {
-  COUNTRY,
-  DESCRIPTION,
-  HAS_TRADING_INCENTIVE,
-  IMG,
-  NAME,
-  TRADE_VOLUME_24H_BTC,
-  TRADE_VOLUME_24H_BTC_NORMALIZED,
-  TRUST_SCORE,
-  TRUST_SCORE_RANK,
-  URL,
-  YEAR_ESTABLISHED,
-} from '../../shared/constants';
-
-export const SUPPORTED_PARAMS = [
-  NAME,
-  YEAR_ESTABLISHED,
-  COUNTRY,
-  DESCRIPTION,
-  HAS_TRADING_INCENTIVE,
-  IMG,
-  TRADE_VOLUME_24H_BTC,
-  TRADE_VOLUME_24H_BTC_NORMALIZED,
-  TRUST_SCORE,
-  TRUST_SCORE_RANK,
-  URL,
-];
 
 export const getExchangeFromURL = () => {
   const allParams = getURLParams();
-  const exchange = {};
-  for (const supportedParam of SUPPORTED_PARAMS) {
-    if (allParams.has(supportedParam))
-      exchange[supportedParam] = allParams.get(supportedParam);
-  }
-  return exchange;
-};
-
-export const getSanitizedExchange = () => {
-  const exchange = getExchangeFromURL();
-  const sanitizedExchange = {};
-  let param;
-  for (let i = 0; i < SUPPORTED_PARAMS.length; i++) {
-    param = SUPPORTED_PARAMS[i];
-    sanitizedExchange[param] = sanitizeParams(param, exchange[param]);
-  }
-  return sanitizedExchange;
+  return allParams.get('id');
 };
